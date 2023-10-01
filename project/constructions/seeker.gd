3ef526d63cea1a8246a242d5ffd638a31e9cb1eb
Alex Gestwicki <alex.gestwicki@gmail.com>
2023-10-01 08:38:17 -0400

    Add enemy and seeker art

--- project/constructions/seeker.gd
+++ project/constructions/seeker.gd
@@ -4,11 +4,17 @@ extends Construction
 var target : Enemy
 var potential_targets := []
 
+@onready var _sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _process(delta:float)->void:
 	if is_instance_valid(target):
 		if global_position.distance_squared_to(target.global_position) > 5.0:
 			global_position = lerp(global_position, target.global_position, 0.9 * delta)
+		if target.global_position.x < global_position.x:
+			_sprite.flip_h = true
+		else:
+			_sprite.flip_h = false
 	else:
 		_find_new_target()
 
--- project/enemies/enemy.gd
+++ project/enemies/enemy.gd
@@ -11,6 +11,7 @@ var _can_damage := true
 var sphere_index := -1
 
 @onready var _cooldown_timer : Timer = $CooldownTimer
+@onready var _sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _process(delta:float)->void:
@@ -19,7 +20,10 @@ func _process(delta:float)->void:
 			global_position = lerp(global_position, target.global_position, 0.4 * delta)
 		if Geometry2D.is_point_in_circle(target.global_position, global_position, 10) and _can_damage:
 			_damage_target()
-		
+		if target.global_position.x < global_position.x:
+			_sprite.flip_h = true
+		else:
+			_sprite.flip_h = false
 	else:
 		_find_new_target()
 
@@ -27,7 +31,9 @@ func _process(delta:float)->void:
 func _find_new_target()->void:
 	_clean_potential_targets()
 	if potential_targets.size() > 0:
-		target = potential_targets.pick_random()
+		var index := randi() % potential_targets.size()
+		if is_instance_valid(potential_targets[index]):
+			target = potential_targets[index]
 
 
 func _clean_potential_targets()->void:
