e0f873cfe1e5b4743ca1d465eb18abaf41f47bcd
Alex Gestwicki <alex.gestwicki@gmail.com>
2023-10-01 17:11:40 -0400

    Add abyssal threads image

--- project/constructions/abyssal_thread.gd
+++ project/constructions/abyssal_thread.gd
@@ -1,2 +1,7 @@
 class_name AbyssalThread
 extends Construction
+
+
+func _ready()->void:
+	modulate = Color(Color.WHITE, 0.0)
+	get_tree().create_tween().set_trans(Tween.TRANS_QUAD).tween_property(self, "modulate", Color.WHITE, 1.0)
